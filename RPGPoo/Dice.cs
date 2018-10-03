0ce3dda51688782afebeb03827f830034849c000
unknown <ickbernardo@gmail.com>
2018-10-02 21:54:14 -0300

    Dice esquecido

---
+++ RPGPoo/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPoo
+{
+    public static class Dice
+    {
+        public static Int32 rollD20()
+        {
+            return new Random().Next(20) + 1;
+        }
+    }
+}
